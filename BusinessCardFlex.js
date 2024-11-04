d9f20ef4f06c3573749ad3381ddf05c1278d3eea
mike800838 <mike800838@gmail.com>
2024-11-05 00:39:35 +0800

    change flex

--- BusinessCardFlex.js
+++ BusinessCardFlex.js
@@ -4,315 +4,302 @@ BusinessCardFlexArray={
 				  'type': 'flex',
 				  'altText': 'this is a flex message',
 				  'contents': {
-	  'type': 'carousel',
-	  'contents': [
-		{
-		  'type': 'bubble',
-		  'body': {
-			'type': 'box',
-			'layout': 'vertical',
-			'contents': [
-			  {
-				'type': 'image',
-				'url': 'https://mike800838.github.io/LineBusinessCard/img/LineBC/XinYua/XinYua1.png',
-				'size': 'full',
-				'aspectMode': 'cover',
-				'aspectRatio': '2:3',
-				'gravity': 'top'
-			  },
-			  {
-				'type': 'box',
-				'layout': 'vertical',
-				'contents': [
-				  {
-					'type': 'box',
-					'layout': 'vertical',
-					'contents': [
-					  {
-						'type': 'text',
-						'text': '陳俊成',
-						'size': 'xl',
-						'color': '#ffffff',
-						'weight': 'bold'
-					  }
-					]
-				  },
-				  {
-					'type': 'box',
-					'layout': 'baseline',
-					'contents': [
-					  {
-						'type': 'text',
-						'text': '台灣茶葉商',
-						'color': '#ebebeb',
-						'size': 'sm',
-						'flex': 0
-					  }
-					],
-					'spacing': 'lg'
-				  },
-				  {
-					'type': 'box',
-					'layout': 'vertical',
-					'contents': [
-					  {
-						'type': 'filler'
-					  },
-					  {
-						'type': 'box',
-						'layout': 'baseline',
-						'contents': [
-						  {
-							'type': 'filler'
-						  },
-						  {
-							'type': 'text',
-							'text': '加入LINE官方帳號',
-							'color': '#ffffff',
-							'flex': 0,
-							'offsetTop': '-2px',
-							'action': {
-							  'type': 'uri',
-							  'label': 'action',
-							  'uri': 'https://lin.ee/K2XGfLl'
-							}
-						  },
-						  {
-							'type': 'filler'
-						  }
-						],
-						'spacing': 'sm'
-					  },
-					  {
-						'type': 'filler'
-					  }
-					],
-					'borderWidth': '1px',
-					'cornerRadius': '4px',
-					'spacing': 'sm',
-					'borderColor': '#ffffff',
-					'margin': 'xxl',
-					'height': '40px'
-				  }
-				],
-				'position': 'absolute',
-				'offsetBottom': '0px',
-				'offsetStart': '0px',
-				'offsetEnd': '0px',
-				'backgroundColor': '#9C8E7Ecc',
-				'paddingAll': '20px',
-				'paddingTop': '18px'
-			  }
-			],
-			'paddingAll': '0px'
-		  }
-		},
-		{
-		  'type': 'bubble',
-		  'body': {
-			'type': 'box',
-			'layout': 'vertical',
-			'contents': [
-			  {
-				'type': 'image',
-				'url': 'https://mike800838.github.io/LineBusinessCard/img/LineBC/XinYua/XinYua2.png',
-				'size': 'full',
-				'aspectMode': 'cover',
-				'aspectRatio': '2:3',
-				'gravity': 'top'
-			  },
-			  {
-				'type': 'box',
-				'layout': 'vertical',
-				'contents': [
-				  {
-					'type': 'box',
-					'layout': 'vertical',
-					'contents': [
-					  {
-						'type': 'text',
-						'text': '各式茶食禮盒',
-						'size': 'xl',
-						'color': '#ffffff',
-						'weight': 'bold'
-					  }
-					]
-				  },
-				  {
-					'type': 'box',
-					'layout': 'baseline',
-					'contents': [
-					  {
-						'type': 'text',
-						'text': '送禮最佳選擇',
-						'color': '#ffffffcc',
-						'decoration': 'none',
-						'gravity': 'bottom',
-						'flex': 0,
-						'size': 'sm'
-					  }
-					],
-					'spacing': 'lg'
-				  },
-				  {
-					'type': 'box',
-					'layout': 'vertical',
-					'contents': [
-					  {
-						'type': 'filler'
-					  },
-					  {
-						'type': 'box',
-						'layout': 'baseline',
-						'contents': [
-						  {
-							'type': 'filler'
-						  },
-						  {
-							'type': 'text',
-							'text': '購買茶葉禮盒',
-							'color': '#ffffff',
-							'flex': 0,
-							'offsetTop': '-2px',
-							'action': {
-							  'type': 'uri',
-							  'label': 'action',
-							  'uri': 'https://sites.google.com/view/xinyueteatop'
-							}
-						  },
-						  {
-							'type': 'filler'
-						  }
-						],
-						'spacing': 'sm'
-					  },
-					  {
-						'type': 'filler'
-					  }
-					],
-					'borderWidth': '1px',
-					'cornerRadius': '4px',
-					'spacing': 'sm',
-					'borderColor': '#ffffff',
-					'margin': 'xxl',
-					'height': '40px'
-				  }
-				],
-				'position': 'absolute',
-				'offsetBottom': '0px',
-				'offsetStart': '0px',
-				'offsetEnd': '0px',
-				'backgroundColor': '#9C8E7Ecc',
-				'paddingAll': '20px',
-				'paddingTop': '18px'
-			  }
-			],
-			'paddingAll': '0px'
-		  }
-		},
-		{
-		  'type': 'bubble',
-		  'body': {
-			'type': 'box',
-			'layout': 'vertical',
-			'contents': [
-			  {
-				'type': 'image',
-				'url': 'https://mike800838.github.io/LineBusinessCard/img/LineBC/XinYua/XinYua3.png',
-				'size': 'full',
-				'aspectMode': 'cover',
-				'aspectRatio': '2:3',
-				'gravity': 'top'
-			  },
-			  {
-				'type': 'box',
-				'layout': 'vertical',
-				'contents': [
-				  {
-					'type': 'box',
-					'layout': 'vertical',
-					'contents': [
-					  {
-						'type': 'text',
-						'text': '多樣化茶葉',
-						'size': 'xl',
-						'color': '#ffffff',
-						'weight': 'bold'
-					  }
-					]
-				  },
-				  {
-					'type': 'box',
-					'layout': 'baseline',
-					'contents': [
-					  {
-						'type': 'text',
-						'text': '任君挑選',
-						'color': '#ebebeb',
-						'size': 'sm',
-						'flex': 0
-					  }
-					],
-					'spacing': 'lg'
-				  },
-				  {
-					'type': 'box',
-					'layout': 'vertical',
-					'contents': [
-					  {
-						'type': 'filler'
-					  },
-					  {
-						'type': 'box',
-						'layout': 'baseline',
-						'contents': [
-						  {
-							'type': 'filler'
-						  },
-						  {
-							'type': 'text',
-							'text': '追蹤心悅FB',
-							'color': '#ffffff',
-							'flex': 0,
-							'offsetTop': '-2px',
-							'action': {
-							  'type': 'uri',
-							  'label': 'action',
-							  'uri': 'https://www.facebook.com/profile.php?id=61561379589466'
-							}
-						  },
-						  {
-							'type': 'filler'
-						  }
-						],
-						'spacing': 'sm'
-					  },
-					  {
-						'type': 'filler'
-					  }
-					],
-					'borderWidth': '1px',
-					'cornerRadius': '4px',
-					'spacing': 'sm',
-					'borderColor': '#ffffff',
-					'margin': 'xxl',
-					'height': '40px'
-				  }
-				],
-				'position': 'absolute',
-				'offsetBottom': '0px',
-				'offsetStart': '0px',
-				'offsetEnd': '0px',
-				'backgroundColor': '#9C8E7Ecc',
-				'paddingAll': '20px',
-				'paddingTop': '18px'
-			  }
-			],
-			'paddingAll': '0px'
-		  }
-		}
-	  ]
-	}
+  "type": "carousel",
+  "contents": [
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "image",
+            "url": "https://mike800838.github.io/LineBusinessCard/img/LineBC/XinYua/XinYua1.png",
+            "size": "full",
+            "aspectMode": "cover",
+            "aspectRatio": "2:3",
+            "gravity": "top"
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "filler"
+                  },
+                  {
+                    "type": "box",
+                    "layout": "baseline",
+                    "contents": [
+                      {
+                        "type": "filler"
+                      },
+                      {
+                        "type": "text",
+                        "text": "分享名片",
+                        "color": "#ffffff",
+                        "flex": 0,
+                        "offsetTop": "-2px",
+                        "action": {
+                          "type": "uri",
+                          "label": "action",
+                          "uri": "https://liff.line.me/2006499149-Z4M3yBR5"
+                        }
+                      },
+                      {
+                        "type": "filler"
+                      }
+                    ],
+                    "spacing": "sm"
+                  },
+                  {
+                    "type": "filler"
+                  }
+                ],
+                "borderWidth": "1px",
+                "cornerRadius": "6px",
+                "spacing": "sm",
+                "borderColor": "#ffffff",
+                "margin": "xxl",
+                "height": "30px"
+              }
+            ],
+            "position": "absolute",
+            "offsetBottom": "0px",
+            "offsetStart": "0px",
+            "offsetEnd": "0px",
+            "backgroundColor": "#9C8E7E",
+            "paddingAll": "10px"
+          }
+        ],
+        "paddingAll": "0px"
+      }
+    },
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "image",
+            "url": "https://mike800838.github.io/LineBusinessCard/img/LineBC/XinYua/XinYua2.png",
+            "size": "full",
+            "aspectMode": "cover",
+            "aspectRatio": "2:3",
+            "gravity": "top"
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "filler"
+                  },
+                  {
+                    "type": "box",
+                    "layout": "baseline",
+                    "contents": [
+                      {
+                        "type": "filler"
+                      },
+                      {
+                        "type": "text",
+                        "text": "加入心悅官方Line",
+                        "color": "#ffffff",
+                        "flex": 0,
+                        "offsetTop": "-2px",
+                        "action": {
+                          "type": "uri",
+                          "label": "action",
+                          "uri": "https://lin.ee/jRipkqn"
+                        }
+                      },
+                      {
+                        "type": "filler"
+                      }
+                    ],
+                    "spacing": "sm"
+                  },
+                  {
+                    "type": "filler"
+                  }
+                ],
+                "borderWidth": "1px",
+                "cornerRadius": "6px",
+                "spacing": "sm",
+                "borderColor": "#ffffff",
+                "margin": "xxl",
+                "height": "30px"
+              }
+            ],
+            "position": "absolute",
+            "offsetBottom": "0px",
+            "offsetStart": "0px",
+            "offsetEnd": "0px",
+            "backgroundColor": "#9C8E7E",
+            "paddingAll": "10px"
+          }
+        ],
+        "paddingAll": "0px"
+      }
+    },
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "image",
+            "url": "https://mike800838.github.io/LineBusinessCard/img/LineBC/XinYua/XinYua3.png",
+            "size": "full",
+            "aspectMode": "cover",
+            "aspectRatio": "2:3",
+            "gravity": "top"
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "filler"
+                  },
+                  {
+                    "type": "box",
+                    "layout": "baseline",
+                    "contents": [
+                      {
+                        "type": "filler"
+                      },
+                      {
+                        "type": "text",
+                        "text": "追蹤心悅FB",
+                        "color": "#ffffff",
+                        "flex": 0,
+                        "offsetTop": "-2px",
+                        "action": {
+                          "type": "uri",
+                          "label": "action",
+                          "uri": "https://www.facebook.com/profile.php?id=61561379589466"
+                        }
+                      },
+                      {
+                        "type": "filler"
+                      }
+                    ],
+                    "spacing": "sm"
+                  },
+                  {
+                    "type": "filler"
+                  }
+                ],
+                "borderWidth": "1px",
+                "cornerRadius": "6px",
+                "spacing": "sm",
+                "borderColor": "#ffffff",
+                "margin": "xxl",
+                "height": "30px"
+              }
+            ],
+            "position": "absolute",
+            "offsetBottom": "0px",
+            "offsetStart": "0px",
+            "offsetEnd": "0px",
+            "backgroundColor": "#9C8E7E",
+            "paddingAll": "10px"
+          }
+        ],
+        "paddingAll": "0px"
+      }
+    },
+    {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "image",
+            "url": "https://mike800838.github.io/LineBusinessCard/img/LineBC/XinYua/XinYua4.png",
+            "size": "full",
+            "aspectMode": "cover",
+            "aspectRatio": "2:3",
+            "gravity": "top"
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                  {
+                    "type": "filler"
+                  },
+                  {
+                    "type": "box",
+                    "layout": "baseline",
+                    "contents": [
+                      {
+                        "type": "filler"
+                      },
+                      {
+                        "type": "text",
+                        "text": "心悅茶行官網",
+                        "color": "#ffffff",
+                        "flex": 0,
+                        "offsetTop": "-2px",
+                        "action": {
+                          "type": "uri",
+                          "label": "action",
+                          "uri": "https://sites.google.com/view/xinyueteatop/%E9%A6%96%E9%A0%81"
+                        }
+                      },
+                      {
+                        "type": "filler"
+                      }
+                    ],
+                    "spacing": "sm"
+                  },
+                  {
+                    "type": "filler"
+                  }
+                ],
+                "borderWidth": "1px",
+                "cornerRadius": "6px",
+                "spacing": "sm",
+                "borderColor": "#ffffff",
+                "margin": "xxl",
+                "height": "30px"
+              }
+            ],
+            "position": "absolute",
+            "offsetBottom": "0px",
+            "offsetStart": "0px",
+            "offsetEnd": "0px",
+            "backgroundColor": "#9C8E7E",
+            "paddingAll": "10px"
+          }
+        ],
+        "paddingAll": "0px"
+      }
+    }
+  ]
+}
 				}
 			],
 			

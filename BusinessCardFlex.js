eed26c065a02d39727648960359751a31496bfc1
Jarvis <jarvis.lee@aoitek.com>
2024-12-02 16:03:17 +0800

    fix apng flex code

--- BusinessCardFlex.js
+++ BusinessCardFlex.js
@@ -16,7 +16,6 @@ BusinessCardFlexArray={
             "type": "image",
             "url": "https://mike800838.github.io/LineBusinessCard/img/LineBC/XinYua/XinYua1_2412020350.apng",
             "size": "full",
-			"animated": "true",
             "aspectMode": "cover",
             "aspectRatio": "2:3",
             "gravity": "top"
